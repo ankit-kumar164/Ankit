ef30b649a295ebb355c797be43d664e407a8c1de
ankit-kumar164 <111178417+ankit-kumar164@users.noreply.github.com>
2023-09-04 18:24:57 +0530

    Update webpack.config.server.js

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -7,9 +7,8 @@ const config = {
     entry: [ path.join(CURRENT_WORKING_DIR , './server/server.js') ],
     target: "node",
     output: {
-        path: path.join(CURRENT_WORKING_DIR , '/dist/'),
+        path: "./",
         filename: "server.generated.js",
-        publicPath: '/dist/',
         libraryTarget: "commonjs2"
     },
     externals: [nodeExternals()],
